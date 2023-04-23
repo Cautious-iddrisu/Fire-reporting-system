e1f3b25be79c622a9e0844bd201de60c82ea994f
cautious <you@example.com>
2023-04-22 20:10:16 -0700

    first commit

---
+++ Readme.txt
@@ -0,0 +1,22 @@
+How to run 
+
+
+
+
+1. Extract the file and copy ofrs folder
+
+2.Paste inside root directory(wamp wamp/www)
+
+3.Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+4. Create a database with the name  ofrsdb
+
+5. Import ofrsdb.sql file
+
+5. Run the script http://localhost/ofrs
+
+6.Access admin http://localhost/admin
+
+Admin Credential
+Username: admin
+Password: admin
---
+++ ofrs/index.php
@@ -0,0 +1,46 @@
+<?php include_once('includes/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Online Fire Reporting system</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body class="bg-success bg">
+        <!-- Responsive navbar-->
+            <?php include_once('includes/header.php') ?>
+            <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center ">
+                <div class="col-lg-12"><img class="img-fluid d-block w-100 h-80 rounded " src="assets/fire.jpg" alt="..." /></div>
+         
+            </div>
+        <!-- Page Content-->
+        <!-- <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+              
+                <div class="col-lg-8">
+                    <h1 class="font-weight-light">Emergency Reporting of Fire</h1>
+                    <p>Together, we continue to empower first responders
+                        with data and insights that drive quality and performance
+                        improvements across the entire health and public safety spectrum.</p>
+                    <a class="btn btn-primary btn-lg" href="reporting.php">Fire Reporting</a>
+                </div>
+            </div>
+     
+        </div> -->
+        <!-- Footer-->
+<?php include_once('includes/footer.php') ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
